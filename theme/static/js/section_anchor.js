d0e39967f9f45c2080682f7d758caf6d2174db35
⟨m∀⟩ <mva.led@gmail.com>
2025-07-26 17:40:16 +0200

    Tweak CSS of section-links.

--- theme/static/js/section_anchor.js
+++ theme/static/js/section_anchor.js
@@ -1,14 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('div.section[id]').forEach(function(section) {
-    var header = section.querySelector('h1, h2, h3, h4, h5, h6');
-    if (header) {
-      var id = section.id;
-      var link = document.createElement('a');
-      link.classList.add('section-link');
-      link.href = '#' + id;
-      link.textContent = header.textContent;
-      header.textContent = '';
-      header.appendChild(link);
-    }
-  });
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('div.section[id]').forEach(function (section) {
+        var header = section.querySelector('h1, h2, h3, h4, h5, h6');
+        if (header) {
+            var id = section.id;
+            var link = document.createElement('a');
+            link.classList.add('section-link');
+            link.href = `#${id}`;
+            link.textContent = header.textContent;
+            header.textContent = '';
+            header.appendChild(link);
+        }
+    });
 });
